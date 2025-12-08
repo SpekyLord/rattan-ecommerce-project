e9a983244534ab0e32597e2e106e67c6c384e4ce
Pish-e <lorinmikaela091906@gmail.com>
2025-12-09 00:00:38 +0800

    Edited product_detail.php and products.php

---
+++ config-20251206T075453Z-3-001/config/db_config.php
@@ -0,0 +1,17 @@
+<?php
+
+define('DB_HOST', 'test-rattan-ecommerce-project.e.aivencloud.com');
+
+
+define('DB_PORT', '26293');
+
+
+define('DB_NAME', 'rattan_shop');
+
+
+define('DB_USER', 'avnadmin');
+
+
+define('DB_PASS', 'AVNS_9v5VX9-lMO5FyLVLoSI');
+
+?>
